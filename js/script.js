2abd6d62a4fc4c7dfa69e6ef02e29c1be3e2a83d
Pablo Pena <lito23.pjp@gmail.com>
2019-01-08 23:19:40 +0000

    first day pushes for playlist

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ["El Nino De La Tuna", "Eres Divina", "A Mis Enemigos", "Mente En Blanco" ];
 // BELOW Add 4 More arrays to store images_links, atists, song lengths, and links for each song
+var images_links = ["https://images-na.ssl-images-amazon.com/images/I/51MraqrcZRL._SS500.jpg", "https://images-na.ssl-images-amazon.com/images/I/51uj8%2BvHTVL._SS500.jpg", "https://images-na.ssl-images-amazon.com/images/I/51oC-hjEd1L._SS500.jpg", "https://images-na.ssl-images-amazon.com/images/I/51w48P7YZlL._SS500.jpg"];
+
+var artists = ["Roberto Tapia", "Patrulla 81", "Valentin Elizalde", "Voz De Mando"];
+
+var song_lengths = ["217", "207", "183", "220"];
+
+var links = ["https://www.youtube.com/watch?v=qV04HIhTEuw", "https://www.youtube.com/watch?v=9kFn7xv18aE", "https://www.youtube.com/watch?v=jBU9cbrbrn0", "https://www.youtube.com/watch?v=zU3p_yTtcfw" ];
 // Make sure they match the same order as your array above
 
 
@@ -10,7 +17,7 @@ var songs = [];
 
     // BELOW Use forEach Loop to display the data from each of your array's in the correct div
 
-}
+
 
 function emptySongInfo(){
     $("#songs").empty();
